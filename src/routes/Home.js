75c25b69037ae9c8dd88aaff9750462708728235
matt.smith <matt.smith@capgemini.com>
2018-09-02 16:11:30 +0100

    Started building up welcome page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHomeImage = styled.img`
+    width: 100%;
+    height: 300px;
+`
+
+const StyledWelcomeText = styled.div`
+    margin-top: 20px;
+    padding: 60px;
+    border: solid 1px #ddd;
+    font-size: 18px;
+    line-height: 24px;
+    white-space: pre-line;
+    font-family: system-ui, Arial, sans-serif;
+`
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>Home</div>
+            <div>
+                <StyledHomeImage />
+                <StyledWelcomeText>
+                    Example Text
+                </StyledWelcomeText>
+            </div>
         )
     }
 }
