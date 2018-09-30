341f568ac97b317248bda5fd2f9df286bd6022b6
matt.smith <matt.smith@capgemini.com>
2018-09-30 21:07:11 +0100

    Implemented a Google Map in the Map component, currently centered on Manchester

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledMap = styled.div`
+    width: 100%;
+    height: calc(70vh);
+`
 
 export default class Map extends React.Component {
+    componentDidMount() {
+        let latlng = new google.maps.LatLng(53.4879778, -2.239773);
+        
+        new google.maps.Map(document.getElementById("map"), {
+            center: latlng,
+            zoom: 8
+        });
+    }
+
     render() {
         return (
-            <div>Map</div>
+            <StyledMap id="map" />
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ initialiseStore();
 
 const appRoot = document.getElementById("app");
 
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`;
+document.head.append(script);
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
