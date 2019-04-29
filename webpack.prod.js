90653f497a13da372fc7a37d65cc981d46afa89e
matt.smith <matt.smith@capgemini.com>
2019-04-29 21:16:05 +0100

    Fix build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const webpack = require("webpack")
 const config = require('./webpack.config.js');
@@ -9,9 +8,8 @@ module.exports = merge(config, {
     mode: "production",
     plugins: [
         new MinifyPlugin(),
-        new UglifyJSPlugin(),
         new CompressionPlugin({
-            asset: "[path].gz[query]",
+            filename: "[path].gz[query]",
             algorithm: "gzip",
             test: /\.js$|\.css$|\.html$/,
             threshold: 10240,
